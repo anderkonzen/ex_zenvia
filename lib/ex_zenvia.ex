b0be2e320fb80cbf3a3a421121248c32b94f5137
Anderson Konzen <anderson.konzen@gmail.com>
2018-02-20 18:57:33 -0300

    Project creation

---
+++ lib/ex_zenvia.ex
@@ -0,0 +1,18 @@
+defmodule ExZenvia do
+  @moduledoc """
+  Documentation for ExZenvia.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExZenvia.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_zenvia_test.exs
@@ -0,0 +1,8 @@
+defmodule ExZenviaTest do
+  use ExUnit.Case
+  doctest ExZenvia
+
+  test "greets the world" do
+    assert ExZenvia.hello() == :world
+  end
+end
